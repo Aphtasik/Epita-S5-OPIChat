f81964e409df0cd7907bdb9b3fb1be9c2dbcd8ee
Mathis Guilbaud <mathis.guilbaud@gmail.com>
2021-11-11 11:07:07 +0100

    feat: basic server first push

---
+++ basic_server/basic_server.c
@@ -0,0 +1,105 @@
+#include "basic_server.h"
+
+#include <err.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int create_and_bind(struct addrinfo *addrinfo)
+{
+    struct addrinfo *rp = addrinfo;
+    int socketfd;
+    for (; rp != NULL; rp = rp->ai_next)
+    {
+        socketfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        int optval = 1;
+        setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+        if (socketfd == -1)
+            continue;
+        if (bind(socketfd, rp->ai_addr, rp->ai_addrlen) == 0)
+            break;
+        close(socketfd);
+    }
+    if (!rp)
+        return 1;
+    return socketfd;
+}
+
+int prepare_socket(const char *ip, const char *port)
+{
+    struct addrinfo addr;
+    struct addrinfo *res;
+    memset(&addr, 0, sizeof(struct addrinfo));
+
+    int ret = getaddrinfo(ip, port, &addr, &res);
+    if (ret != 0)
+        errx(EXIT_FAILURE, "getaddrinfo error");
+    int socket = create_and_bind(res);
+    freeaddrinfo(res);
+    return socket;
+}
+
+void rewrite(int fd, const void *buf, size_t count, int print)
+{
+    if(print)
+        write(1, "Received Body: ", sizeof("Received Body: "));
+    ssize_t x = 0;
+    if ((x = write(fd, buf, count)) == -1)
+    {
+        errx(1, "write function exit with error code: %d", errno);
+    }
+    else if ((size_t) x != count)
+    {
+        while ((size_t) x < count)
+        {
+            x += write(fd, buf, 1);
+        }
+    }
+}
+
+void echo(int fd_in, int fd_out)
+{
+    size_t nread;
+    void *buffer[DEFAULT_BUFFER_SIZE];
+    while ((nread = read(fd_in, buffer, DEFAULT_BUFFER_SIZE - 1)) > 0)
+    {
+        rewrite(1, buffer, nread, 1);
+        rewrite(fd_out, buffer, nread, 0);
+    }
+}
+
+int accept_client(int socket)
+{
+    struct sockaddr peer_addr;
+    socklen_t peer_len;
+    listen(socket, 5);
+    int connection = accept(socket, &peer_addr, &peer_len);
+    if (connection == -1)
+        errx(EXIT_FAILURE, "accept connection failed");
+    close(socket);
+    write(1, "Client connected\n", sizeof("Client connected\n"));
+    return connection;
+}
+
+void communicate(int client_socket)
+{
+    echo(client_socket, client_socket);
+    close(client_socket);
+    rewrite(1, "Client disconnected\n", sizeof("Client disconnected"), 0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+        printf("Usage: <host> <port>");
+    int socket = prepare_socket(argv[1], argv[2]);
+    if(socket == 1)
+        errx(EXIT_FAILURE, "socket creation error");
+    int server_socket = accept_client(socket);
+    communicate(server_socket);
+}
