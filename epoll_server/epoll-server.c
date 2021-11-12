dfa53ddfdd0af86c2752a3c82c984b9e0500f60f
Mathis Guilbaud <mathis.guilbaud@gmail.com>
2021-11-12 22:23:25 +0100

    test something

--- epoll_server/epoll-server.c
+++ epoll_server/epoll-server.c
@@ -144,7 +144,15 @@ int main(int argc, char **argv)
                         if (nsend == -1)
                             errx(EXIT_FAILURE, "NOSIGNAL error %d", errno);
                         else if (nsend == 0)
-                            break;
+                        {
+                            // Client error / want to disconnect => disconnect
+                            if (epoll_ctl(epoll_instance, EPOLL_CTL_DEL, sock,
+                                        &events[event_idx]))
+                                errx(EXIT_FAILURE, "epoll ctl failure %d", errno);
+                            client_list = remove_client(client_list, sock);
+                            close(sock);
+                            write(1, "Cient Disconnected\n", 19);
+                        }
 
                         tmp = tmp->next;
                     }
